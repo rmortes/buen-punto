c40608fd3d2b149f5611a0390cf75fcff783c1c6
rmortes <56652057+rmortes@users.noreply.github.com>
2025-01-25 00:10:24 +0100

    worked in movement

--- assets/player/body.gd
+++ assets/player/body.gd
@@ -1,8 +1,12 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var acceleration: float = 2.0
+@export var terminal_speed: float = 15.0
+@export var time_to_terminal: float = 3.0
+@export var jump_velocity = 4.5
+
+var time_accelerating = 0;
 
 # TODO: Esto lo podemos poner en las settings cuando hayan
 var mouse_sensitivity = 0.002
@@ -15,18 +19,25 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
+	
+	if Input.is_action_pressed("ui_up"):
+		time_accelerating = min(time_accelerating + delta, time_to_terminal)
+	else:
+		time_accelerating = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var current_acceleration = min(terminal_speed, terminal_speed ** (time_accelerating / time_to_terminal))
+
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_acceleration
+		velocity.z = direction.z * current_acceleration
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_acceleration)
+		velocity.z = move_toward(velocity.z, 0, current_acceleration)
 
 	move_and_slide()
 
