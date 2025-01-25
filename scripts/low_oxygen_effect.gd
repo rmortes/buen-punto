0eef1f3eee13e94c9440ff6880154a16e33a5bd7
DanganRodri <rodio.rps@gmail.com>
2025-01-25 14:07:39 +0100

    added danger fade out effect

---
+++ scripts/low_oxygen_effect.gd
@@ -0,0 +1,24 @@
+extends ColorRect
+@onready var oxygen_timer = %OxygenTimer
+
+var danger_threshold_start : float = 15.0
+var last_moments_left : float = 3.0
+var max_value : float = 3.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var time_left = oxygen_timer.time_left - last_moments_left
+	if time_left < danger_threshold_start and time_left > 0.0:
+		var value = calculate_increment(time_left, danger_threshold_start, max_value)
+		self.material.set_shader_parameter("EffectStrength", value)
+
+func calculate_increment(time_left: float, time_start: float, max_value: float) -> float:
+	# Calcula la proporción del tiempo restante
+	var t = clamp(1.0 - (time_left / time_start), 0.0, 1.0)
+	# Interpola el valor basado en la proporción
+	return lerp(0.0, max_value, t)
