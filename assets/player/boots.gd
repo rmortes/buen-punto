498df5f63acff1e08d0dafa66a9413237575dbed
rmortes <56652057+rmortes@users.noreply.github.com>
2025-01-25 19:28:12 +0100

    dynamic steps

--- assets/player/boots.gd
+++ assets/player/boots.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Timer
 
 # The audios commented are not needed
 const steps = [
@@ -15,11 +15,27 @@ const steps = [
 	#preload("res://assets/paso-11.mp3"),
 ];
 
+@onready var character_body_3d: CharacterBody3D = $"../CharacterBody3D"
+
+func easingFunction(x: float) -> float: 
+	var c1 = 1.70158;
+	var c3 = c1 + 1;
+
+	return 1 - (1 - x) * (1 - x)
+
+
+func _process(delta: float) -> void:
+	var speed := character_body_3d.velocity.length() / 15
+	var interval : float = lerp(4, 1, speed) / 4
+	print("interval: " + str(interval))
+	print("easing: " + str(easingFunction(interval)))
+	wait_time = easingFunction(interval) / 2
+
 func _on_timeout() -> void:
 	if (Input.is_action_pressed("ui_up")
 		or Input.is_action_pressed("ui_down")
 		or Input.is_action_pressed("ui_left")
 		or Input.is_action_pressed("ui_right")
-		):
+		) and character_body_3d.is_on_floor():
 		var step = randi_range(0, len(steps)-1)
 		SoundManager.play_sound(steps[step])
