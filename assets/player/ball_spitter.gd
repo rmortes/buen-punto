06a4747d4178833357e8ff58bb2931a17fd82baf
rmortes <56652057+rmortes@users.noreply.github.com>
2025-01-25 19:50:31 +0100

    sfx works

--- assets/player/ball_spitter.gd
+++ assets/player/ball_spitter.gd
@@ -22,7 +22,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("game_ball"):
 		currentPhantomBall = PHANTOM_BALL.instantiate()
 		call_deferred("add_child",currentPhantomBall)
-	if Input.is_action_pressed("game_ball"):
+	if Input.is_action_pressed("game_ball") and currentPhantomBall != null:
 		var camera_vector := camera_3d.get_global_transform().basis.z
 		var ball_position := camera_3d.global_position - camera_vector * placement_distance
 		currentPhantomBall.position = ball_position
@@ -31,4 +31,5 @@ func _process(delta: float) -> void:
 		var new_ball = GRAV_BALL.instantiate()
 		new_ball.global_position = currentPhantomBall.global_position
 		currentPhantomBall.queue_free()
+		currentPhantomBall = null
 		call_deferred("add_child",new_ball)
