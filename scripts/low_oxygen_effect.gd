c217e244fbdd57852ca7fc77f60169a5dc1b7467
rmortes <56652057+rmortes@users.noreply.github.com>
2025-01-26 00:18:33 +0100

    choke me harder

--- scripts/low_oxygen_effect.gd
+++ scripts/low_oxygen_effect.gd
@@ -1,12 +1,14 @@
 extends ColorRect
 @onready var oxygen_timer = %OxygenTimer
 
-@export var danger_threshold_start : float = 15.0
-@export var last_moments_left : float = 3.0
+@export var danger_threshold_start : float = 10.0
+@export var last_moments_left : float = 5.0
 @export var max_value : float = 3.0
 @export var can_die := true;
 @onready var player = $"../../../.."
 
+const RESPIMAL_CRUNCHY = preload("res://assets/sounds/respimal crunchy.wav")
+var has_groaned := false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.material.set_shader_parameter("EffectStrength", 0)
@@ -15,6 +17,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var time_left = oxygen_timer.time_left - last_moments_left
+	
+	if oxygen_timer.time_left < last_moments_left:
+		if not has_groaned:
+			SoundManager.play_sound(RESPIMAL_CRUNCHY, "Voice").finished.connect(func(): $"../Subtitulos".text = '')
+			$"../Subtitulos".text = "*chokes in Russian*"
+			has_groaned = true
+	else:
+		has_groaned = false
+		
 	if time_left < danger_threshold_start and time_left > 0.0:
 		var value = calculate_increment(time_left, danger_threshold_start, max_value)
 		self.material.set_shader_parameter("EffectStrength", value if can_die else 0)
